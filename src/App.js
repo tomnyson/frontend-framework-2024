678756ff1e47dade219db6649c418217f2804920
tomnyson <tabletkindfire@gmail.com>
2024-03-07 13:21:55 +0700

    add source

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
      <h1>Hello frontend framework update</h1>
-     <img alt='' src="https://www.ceepeespices.in/wp-content/uploads/2021/02/hing.png"/>
-     <Profile/>
+     {/* <img alt='' src="https://www.ceepeespices.in/wp-content/uploads/2021/02/hing.png"/>
+     <Profile/> */}
     </div>
   );
 }
