a81ea2f51ce1acf005a6eef73fc9be8b435d3b74
tomnyson <tabletkindfire@gmail.com>
2024-03-07 12:25:27 +0700

    add source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Profile from './components/Profile'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello frontend framework</h1>
+     <img alt='' src="https://www.ceepeespices.in/wp-content/uploads/2021/02/hing.png"/>
+     <Profile/>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+/**
+ * ten, mssv, ngay, tuoi, so thich
+ * 
+ */
+function Profile() {
+    return (
+      <div className="Profile">
+       
+       <div className="Profile">
+        <img alt='' src="https://www.ceepeespices.in/wp-content/uploads/2021/02/hing.png" className="image"/>
+        <div className="information">
+            <h2>Ten:QA</h2>
+            <h2>MSSV:PK02832</h2>
+            <h2>Tuoi:20</h2>
+            <h2>Ngay:23/02/2024</h2>
+        </div>
+       </div>
+       
+
+      </div>
+    );
+  }
+  
+  export default Profile;
