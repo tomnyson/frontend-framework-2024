216f2e220e6281571271fd2065efc3beba1188ba
tomnyson <tabletkindfire@gmail.com>
2024-03-14 13:53:27 +0700

    crud

---
+++ src/DemoAPI.js
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
+const DemoAPI = () => {
+  const [formData, setFormData] = useState({ title: "", image: "", description: "" })
+
+  const [isEdit, setIsEdit] = useState(false)
+
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetchData()
+  }, [])
+  const fetchData = () => {
+    axios
+      .get("http://localhost:3000/posts")
+      .then(function (response) {
+        // handle success
+        setPosts(response.data)
+        console.log(response)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+      .finally(function () {
+        // always executed
+      })
+  }
+
+  const createPost = () => {
+    /**
+     * axios.post('/user', {
+    lastName: 'Trần'
+    firstName: 'Quốc Tuấn',
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+     */
+
+    if (isEdit) {
+      console.log(formData);
+      axios
+      .put(`http://localhost:3000/posts/${formData.id}`, {
+        ...formData,
+      })
+      .then(function (response) {
+        alert("edit success")
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    } else {
+      axios
+        .post("http://localhost:3000/posts", {
+          ...formData,
+        })
+        .then(function (response) {
+          alert("add to success")
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
+
+  function hangleChangeData(e) {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  function handleEdit(item) {
+    setIsEdit(true)
+    setFormData({
+      ...item,
+    })
+    // axios.get(`http://localhost:3000/posts/${id}`)
+    // .then(function(response) {
+    //   console.log(response);
+    // }).catch(function(err) {
+    //   console.log(err);
+    // });
+  }
+
+  const reload = () => {
+    fetchData();
+  }
+
+  function handleDelete(item){
+    axios
+    .delete(`http://localhost:3000/posts/${item.id}`)
+    .then(function (response) {
+      // handle success
+      alert("delete successfully")
+      reload();
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+    .finally(function () {
+      // always executed
+    })
+
+  }
+  return (
+    <div className="container">
+      <div className="flex">
+        <form>
+          <h1>Create post</h1>
+          <input
+            type="text"
+            name="title"
+            defaultValue={formData.title}
+            placeholder="title"
+            id="title"
+            onChange={hangleChangeData}
+          />
+          <input
+            type="text"
+            name="image"
+            defaultValue={formData.image}
+            placeholder="image link"
+            id="image"
+            onChange={hangleChangeData}
+          />
+          <textarea
+            rows={5}
+            defaultValue={formData.description}
+            name="description"
+            placeholder="description"
+            onChange={hangleChangeData}
+          />
+          <button onClick={createPost} type="button">
+            {isEdit ? "edit" : "create"}
+          </button>
+        </form>
+        <div className="posts">
+          {posts.map((item) => {
+            return (
+              <div className="item">
+                <img src={item.image} alt="" />
+                <h2 className="title">{item.title}</h2>
+                <p className="description">{item.description}</p>
+                <button className="btn" onClick={() => handleEdit(item)}>
+                  edit
+                </button>
+                <button className="btn btn-delete" onClick={() => handleDelete(item)}>
+                  delete
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+export default DemoAPI
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,17 +2,18 @@
  * ten, mssv, ngay, tuoi, so thich
  * 
  */
-function Profile() {
+function Profile(props) {
+  console.log(props.ten);
+  console.log(props);
     return (
       <div className="Profile">
-       
        <div className="Profile">
         <img alt='' src="https://www.ceepeespices.in/wp-content/uploads/2021/02/hing.png" className="image"/>
         <div className="information">
-            <h2>Ten:QA</h2>
-            <h2>MSSV:PK02832</h2>
-            <h2>Tuoi:20</h2>
-            <h2>Ngay:23/02/2024</h2>
+            <h2>Ten: {props.ten}</h2>
+            <h2>MSSV:{props.mssv}</h2>
+            <h2>Tuoi:{props.tuoi}</h2>
+            <h2>Ngay: {props.ngay}</h2>
         </div>
        </div>
        
